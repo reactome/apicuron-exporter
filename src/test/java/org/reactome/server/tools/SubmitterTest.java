201861ceba6296a25f3f66338fb6d6ac2770ccdf
EliotRagueneau <eragueneau@ebi.ac.uk>
2023-12-05 16:17:52 +0000

    :white_check_mark: Make sure to not send any data when running tests automatically

--- src/test/java/org/reactome/server/tools/SubmitterTest.java
+++ src/test/java/org/reactome/server/tools/SubmitterTest.java
@@ -1,6 +1,7 @@
 package org.reactome.server.tools;
 
 import org.apache.http.HttpResponse;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,7 +18,7 @@ public class SubmitterTest {
     @Autowired
     public Exporter exporter;
     @Test
-//    @Disabled
+    @Disabled
     public void testSubmit() throws IOException {
         File reports = new File(Objects.requireNonNull(ExporterTest.class.getResource("/reports.json")).getFile());
         HttpResponse response = exporter.submitReports(reports);
